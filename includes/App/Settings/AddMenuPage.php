58293d457f07864d50f6475faeb3e766eb4e6125
hasankarim18 <hallysust@gmail.com>
2025-10-16 09:26:52 +0600

    admin foundation

--- includes/App/Settings/AddMenuPage.php
+++ includes/App/Settings/AddMenuPage.php
@@ -12,33 +12,62 @@ class AddMenuPage
     use \Hasan\ProductDiscountsForWoo\App\Traits\Singleton;
     public function init()
     {
-        $this->add_menu_page();
+        if (is_admin()) {
+            add_action('admin_menu', [$this, 'add_admin_menu']);
+            add_action('admin_init', [$this, 'register_settings']);
+        }
     }
 
-    public function add_menu_page()
+    function add_admin_menu()
     {
-        add_menu_page(
-            'product-discounts-for-woo',
-            'P. D. for WC.',
+        add_submenu_page(
+            'woocommerce',
+            'Discount settings',
+            'Discount settings',
             'manage_options',
-            'pdfw',
-            [$this, 'add_settings_page'],
-            '',
-            100
-
+            'discount-settings',
+            [$this, 'settings_page']
         );
-
     }
 
-    public function add_settings_page()
+    function register_settings()
+    {
+        register_setting('wc_discount_settings', 'wc_discount_min_quantity');
+        register_setting('wc_discount_settings', 'wc_discount_percent');
+    }
+
+
+    function settings_page()
     {
         ?>
         <div class="wrap">
-            <h1>Product Discounts for WooCommerce</h1>
+            <h2>Discount settings</h2>
+            <form method="post" action="options.php">
+                <?php settings_fields('wc_discount_settings'); ?>
+                <table class="form-table">
+                    <tr>
+                        <th scope="row"><?php _e('Minimum quantity', 'pdfw-domain') ?></th>
+                        <td>
+                            <input type="number" name="wc_discount_min_quantity"
+                                value="<?php echo esc_attr(get_option('wc_discount_min_quantity', 2)); ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Discount Percent</th>
+                        <td>
+                            <input type="number" name="wc_discount_percent"
+                                value="<?php echo esc_attr(get_option('wc_discount_percent', 10)); ?>">
+                        </td>
+                    </tr>
+                </table>
+                <?php
+                submit_button();
+                ?>
+            </form>
         </div>
         <?php
-
-
     }
+
+
 
 }
--- includes/App/Settings/CalculateDiscounts/CalculateDiscounts.php
+++ includes/App/Settings/CalculateDiscounts/CalculateDiscounts.php
@@ -33,18 +33,26 @@ class CalculateDiscounts
         // apply_filters('pdfw_product_discount_for_3', 15);
         // apply_filters('pdfw_product_discount_for_2', 10);
 
-        $discount_for_2 = apply_filters('pdfw_product_discount_for_2', 10);
-        $discount_for_3 = apply_filters('pdfw_product_discount_for_3', 15);
+        // $discount_for_2 = apply_filters('pdfw_product_discount_for_2', get_option('wc_discount_percent', 10));
+        // $discount_for_3 = apply_filters('pdfw_product_discount_for_3', 15);
 
 
-        if ($total > 1 && $total < 3) {
-            $discount = $cart->get_subtotal() * $discount_for_2 / 100;
-            $cart->add_fee("{$discount_for_2}% Discount for 2", -$discount);
-        } elseif ($total >= 3) {
-            $discount = $cart->get_subtotal() * $discount_for_3 / 100;
-            $cart->add_fee("{$discount_for_3}% Discount for 3", -$discount);
+        // if ($total > 1 && $total < 3) {
+        //     $discount = $cart->get_subtotal() * $discount_for_2 / 100;
+        //     $cart->add_fee("{$discount_for_2}% Discount for 2", -$discount);
+        // } elseif ($total >= 3) {
+        //     $discount = $cart->get_subtotal() * $discount_for_3 / 100;
+        //     $cart->add_fee("{$discount_for_3}% Discount for 3", -$discount);
+        // }
+
+        if ($total >= 2) {
+            $discount_percent = get_option('wc_discount_percent');
+            $quantity = get_option('wc_discount_min_quantity');
+
+            $discount = $cart->get_subtotal() * $discount_percent / 100;
+            $cart->add_fee("{$discount_percent}% Discount for {$quantity}", -$discount);
         }
-    }
 
+    }
 
 }
